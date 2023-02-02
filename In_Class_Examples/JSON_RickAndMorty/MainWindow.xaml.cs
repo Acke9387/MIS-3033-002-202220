f13c78c0849b2b24a07f43bf94fdc2a66ae5bc6d
Ackerman, Adam P <adam@ou.edu>
2023-02-02 16:06:08 -0600

    Did in class participation over rick and morty

---
+++ In_Class_Examples/JSON_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            RickAndMortyAPI api;
+            using (HttpClient client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+            }
+
+
+            foreach (ResultItem character in api.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+
+
+        }
+
+        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            ResultItem selectedCharacter = (ResultItem)cboCharacters.SelectedItem;
+
+            txtName.Text = selectedCharacter.name;
+            imgPicture.Source = new BitmapImage(new Uri(selectedCharacter.image));
+
+        }
+    }
+}
---
+++ In_Class_Examples/JSON_RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_RickAndMorty
+{
+    public class RickAndMortyAPI
+    {
+
+        public Information info { get; set; }
+
+        public List<ResultItem> results { get; set; }
+    }
+
+    public class ResultItem
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class Information
+    {
+        public int count { get; set; }
+
+        public int pages { get; set; }
+
+        public string next { get; set; }
+
+        public string prev { get; set; }
+
+    }
+}
