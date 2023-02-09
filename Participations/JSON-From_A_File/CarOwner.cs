5aa49b797371ca25c5aa9fbac4cce3f4b7460b3f
Ackerman, Adam P <adam@ou.edu>
2023-02-09 15:21:52 -0600

    Completed json from a file participation

---
+++ Participations/JSON-From_A_File/CarOwner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_From_A_File
+{
+    public class CarOwner
+    {
+
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+        public string Make{get;set;}
+        public string Model{get;set;}
+        public int Year{get;set;}
+        public string Color { get; set; }
+
+        public CarOwner()
+        {
+            FirstName = string.Empty;
+            LastName    = string.Empty;
+            Make        = string.Empty;
+            Model       = string.Empty;
+            Year        = 0;
+            Color       = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} {Color}";
+        }
+
+    }
+}
---
+++ Participations/JSON-From_A_File/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_From_A_File
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<CarOwner> data = new List<CarOwner>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string json = File.ReadAllText("Mock_Data_Car_Owners.json");
+
+
+            data = JsonConvert.DeserializeObject<List<CarOwner>>(json);
+
+            cboColors.Items.Add("All");
+            cboColors.SelectedIndex = 0;
+
+            foreach (var item in data)
+            {
+                string answer = "";
+                if (cboColors.Items.Contains(item.Color) == false)
+                {
+                    cboColors.Items.Add(item.Color); 
+                }
+
+                lstData.Items.Add(item);
+            }
+
+        }
+
+        private void cboColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedColor = (string)cboColors.SelectedItem;
+
+            lstData.Items.Clear();
+            foreach (var item in data)
+            {
+                if (item.Color == selectedColor)
+                {
+                    lstData.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string dataASJSON = JsonConvert.SerializeObject(lstData.Items);
+
+            File.WriteAllText($"{cboColors.SelectedItem.ToString()}_Filtered_Car_Owner.json", dataASJSON);
+
+            MessageBox.Show("Successfully Saved");
+
+        }
+    }
+}
