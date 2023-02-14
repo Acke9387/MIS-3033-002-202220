8887212b59d898feba1339d90ce8ec697f12aee5
Ackerman, Adam P <adam@ou.edu>
2023-02-14 15:30:14 -0600

    started GOT quote

---
+++ In_Class_Examples/JSON-GameOfThronesQuote/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_GameOfThronesQuote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://api.gameofthronesquotes.xyz/v1/random").Result;
+
+            }
+
+        }
+    }
+}
