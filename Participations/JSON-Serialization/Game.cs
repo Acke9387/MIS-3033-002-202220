2a4dcbdf18f81f960481fee4e86f6b67ef58f64e
Ackerman, Adam P <adam@ou.edu>
2023-02-14 15:25:45 -0600

    completed participation over serialization

---
+++ Participations/JSON-Serialization/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Serialization
+{
+    public class Game
+    {
+        public string name { get; set; }
+        public string platform { get; set; }
+        public DateTime? release_date { get; set; }
+        public string summary { get; set; }
+        public int meta_score { get; set; }
+        public string user_review { get; set; }
+
+
+        public Game()
+        {
+            name = string.Empty;
+            platform = string.Empty;
+            release_date = null;
+            summary = string.Empty;
+            meta_score = 0;
+            user_review = String.Empty;
+        }
+
+        public Game(string game)
+        {
+            var piecesOfGame = game.Split(",");
+            //name,platform,release_date,summary,meta_score,user_review
+
+            name = piecesOfGame[0];
+            platform = piecesOfGame[1];
+            release_date = Convert.ToDateTime(piecesOfGame[2]);
+            summary = piecesOfGame[3];
+            meta_score = Convert.ToInt32(piecesOfGame[4]);
+            user_review = piecesOfGame[5];
+        }
+
+        public override string ToString()
+        {
+            return $"{name} - {platform}";
+        }
+    }
+}
---
+++ Participations/JSON-Serialization/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Serialization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Game> Data = new List<Game>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] linesOfFile = File.ReadAllLines("all_games.csv");
+
+            //for (int i = 1; i < linesOfFile.Length; i++)
+            //{
+
+            //}
+
+
+            foreach (var game in linesOfFile.Skip(1))
+            {
+                Game g = new Game(game);
+
+                if (!cboPlatform.Items.Contains(g.platform))
+                {
+                    cboPlatform.Items.Add(g.platform); 
+                }
+
+                Data.Add(g);
+                lstData.Items.Add(g);
+            }
+        }
+
+        private void cboPlatform_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedPlatform = cboPlatform.SelectedItem.ToString();
+
+            lstData.Items.Clear();
+            foreach (var item in Data)
+            {
+                if (item.platform == selectedPlatform)
+                {
+                    lstData.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonOutput = JsonConvert.SerializeObject(lstData.Items);
+            //List<Game> games = JsonConvert.DeserializeObject<List<Game>>(jsonOutput);
+
+            File.WriteAllText($"{cboPlatform.SelectedItem.ToString()}_Games.json", jsonOutput);
+
+            MessageBox.Show("Saved successfully");
+        }
+    }
+}
