b27e390c129ffd47605bcce390a3939dfd748857
Ackerman, Adam P <adam@ou.edu>
2023-04-13 16:17:05 -0500

    started participation

---
+++ Participations/MVC-Beginner/Controllers/ChuckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Beginner.Models;
+using Newtonsoft.Json;
+
+namespace MVC_Beginner.Controllers
+{
+    public class ChuckController : Controller
+    {
+        public IActionResult Index()
+        {
+            ChuckAPI result;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://api.chucknorris.io/jokes/random").Result;
+
+                result = JsonConvert.DeserializeObject<ChuckAPI>(json);
+            }
+
+            return View(result);
+        }
+    }
+}
---
+++ Participations/MVC-Beginner/Models/ChuckAPI.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Beginner.Models
+{
+    public class ChuckAPI
+    {
+
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+    }
+}
